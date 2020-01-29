f9782c6e22e29a9c650a926eb4be578789d31332
Sebastian- <hmurgu@hotmail.com>
2020-01-29 12:57:03 -0800

    initialize products model and database

---
+++ ProductsAPI/Data/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAPI.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext (DbContextOptions<ProductContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
